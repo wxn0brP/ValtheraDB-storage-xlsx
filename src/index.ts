72275e4d615862b8479e1f7463a5df9f0d39000a
wxn0brP <viviennevaltheris@gmail.com>
2026-08-20 19:03:21 +0200

    refactor: add biome

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { ValtheraClass } from "@wxn0brp/db-core";
 import { DbStorageXlsx, Opts } from "./action";
 
 export function createXlsxValthera(opts: Opts) {
-    const xlsxStorage = new DbStorageXlsx(opts);
-    return new ValtheraClass({
-        adapter: xlsxStorage
-    });
+	const xlsxStorage = new DbStorageXlsx(opts);
+	return new ValtheraClass({
+		adapter: xlsxStorage,
+	});
 }
 
 export const DYNAMIC = {
-    xlsx: (opts: Opts) => new DbStorageXlsx(opts)
+	xlsx: (opts: Opts) => new DbStorageXlsx(opts),
 };
